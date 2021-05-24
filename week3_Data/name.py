1e5e776464734800ebe7674815095e0b1c7ee7f4
Alexies_he <jiajun_he1999@163.com>
2021-05-24 21:17:06 +0800

    5.24周一，晚上9:16，Python数据类型复习

---
+++ week3_Data/name.py
@@ -0,0 +1,9 @@
+name = "alexies he jia jun"
+print(name.title())#.title()是对变量name执行方法操作，遇到空格后大写
+name = "alexies he"
+print(name.upper())#.upper()是全大写
+first_name = "alexies"
+family_name = "he"
+name = first_name + " " + family_name
+print("Hello!\n",name.title())
+import this
